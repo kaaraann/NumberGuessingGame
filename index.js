847e08536f0feabe4413f5b9d6610995d6f7e5ce
Karan Thakur <121843663+kaaraann@users.noreply.github.com>
2024-01-21 12:07:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+// A Number guessing game ;
+const minNum = 1;
+const maxNum = 100;
+const ans = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
+
+let attempts = 0;
+let guess;
+let running = true;
+
+while (running) {
+  guess = window.prompt(`Guess the Number Between  ${minNum} - ${maxNum}`);
+  guess = Number(guess);
+
+  if (isNaN(guess)) {
+    window.alert("Please enter a Only valid Number");
+  } else if (guess < minNum || guess > maxNum) {
+    window.alert("Please enter a valid Number");
+  } else {
+    attempts++;
+    if (guess < ans) {
+      window.alert("Too low! try again");
+    } else if (guess > ans) {
+      window.alert("Too High! try again");
+    } else {
+      window.alert(
+        `You are correct answer ${ans}. it took you ${attempts} attempts`
+      );
+      running = false;
+    }
+  }
+}
